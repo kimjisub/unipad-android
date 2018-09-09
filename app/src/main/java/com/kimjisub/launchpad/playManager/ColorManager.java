fba5d011bba0a2b18516d904da4b94061351c073
김지섭 <0226daniel@gmail.com>
2018-09-09 18:36:48 +0900

    ColorManager 외부로 뺌

---
+++ app/src/main/java/com/kimjisub/launchpad/playManager/ColorManager.java
@@ -0,0 +1,66 @@
+package com.kimjisub.launchpad.playManager;
+
+public class ColorManager {
+	public static final int GUIDE = 0;
+	public static final int PRESSED = 1;
+	public static final int LED = 2;
+	
+	Item[][][] btn;
+	Item[][] cir;
+	
+	public static class Item {
+		public int x;
+		public int y;
+		public int chanel;
+		public int color;
+		public int code;
+		
+		Item(int x, int y, int chanel, int color, int code) {
+			this.x = x;
+			this.y = y;
+			this.chanel = chanel;
+			this.color = color;
+			this.code = code;
+		}
+	}
+	
+	public ColorManager(int x, int y) {
+		btn = new Item[x][y][3];
+		cir = new Item[36][3];
+	}
+	
+	public Item get(int x, int y) {
+		Item ret = null;
+		if (x != -1) {
+			for (int i = 0; i < 3; i++) {
+				if (btn[x][y][i] != null) {
+					ret = btn[x][y][i];
+					break;
+				}
+			}
+		} else {
+			for (int i = 0; i < 3; i++) {
+				if (cir[y][i] != null) {
+					ret = cir[y][i];
+					break;
+				}
+			}
+		}
+		return ret;
+	}
+	
+	public void add(int x, int y, int chanel, int color, int code) {
+		if (x != -1)
+			btn[x][y][chanel] = new Item(x, y, chanel, color, code);
+		else
+			cir[y][chanel] = new Item(x, y, chanel, color, code);
+	}
+	
+	public void remove(int x, int y, int chanel) {
+		if (x != -1)
+			btn[x][y][chanel] = null;
+		else
+			cir[y][chanel] = null;
+	}
+	
+}
