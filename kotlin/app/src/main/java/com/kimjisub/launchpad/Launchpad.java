e3774b4214ea3abeb8dab75132722b91649febca
김지섭 <0226daniel@gmail.com>
2018-10-01 08:54:09 +0900

    java clone

---
+++ kotlin/app/src/main/java/com/kimjisub/launchpad/Launchpad.java
@@ -0,0 +1,446 @@
+package com.kimjisub.launchpad;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.kimjisub.launchpad.manage.LaunchpadDriver;
+import com.kimjisub.launchpad.manage.SettingManager;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+import static com.kimjisub.launchpad.Launchpad.MidiDevice.MK2;
+import static com.kimjisub.launchpad.Launchpad.MidiDevice.MidiFighter;
+import static com.kimjisub.launchpad.Launchpad.MidiDevice.Piano;
+import static com.kimjisub.launchpad.Launchpad.MidiDevice.Pro;
+import static com.kimjisub.launchpad.Launchpad.MidiDevice.S;
+import static com.kimjisub.launchpad.manage.Tools.log;
+import static com.kimjisub.launchpad.manage.Tools.logRecv;
+import static com.kimjisub.launchpad.manage.Tools.logSig;
+
+public class Launchpad extends BaseActivity {
+	
+	RelativeLayout RL_err;
+	TextView TV_info;
+	LinearLayout[] LL_Launchpad;
+	LinearLayout[] LL_mode;
+	
+	
+	static UsbManager usbManager;
+	
+	static UsbDevice usbDevice;
+	static UsbInterface usbInterface;
+	static UsbEndpoint usbEndpoint_in;
+	static UsbEndpoint usbEndpoint_out;
+	static UsbDeviceConnection usbDeviceConnection;
+	static boolean isRun = false;
+	
+	public enum MidiDevice {
+		S(0), MK2(1), Pro(2), MidiFighter(3), Piano(4);
+		
+		private final int value;
+		
+		MidiDevice(int value) {
+			this.value = value;
+		}
+	}
+	
+	static MidiDevice device = null;
+	static int mode = 0;
+	static LaunchpadDriver.DriverRef driver = new LaunchpadDriver.Nothing();
+	@SuppressLint("StaticFieldLeak")
+	private static Activity driverFrom = null;
+	private static LaunchpadDriver.DriverRef.OnConnectionEventListener onConnectionEventListener;
+	private static LaunchpadDriver.DriverRef.OnGetSignalListener onGetSignalListener;
+	private static LaunchpadDriver.DriverRef.OnSendSignalListener onSendSignalListener;
+	
+	
+	@SuppressLint({"CutPasteId", "StaticFieldLeak"})
+	void initVar() {
+		RL_err = findViewById(R.id.err);
+		TV_info = findViewById(R.id.info);
+		LL_Launchpad = new LinearLayout[]{
+			findViewById(R.id.s),
+			findViewById(R.id.mk2),
+			findViewById(R.id.pro),
+			findViewById(R.id.midifighter),
+			findViewById(R.id.piano)
+		};
+		LL_mode = new LinearLayout[]{
+			findViewById(R.id.speedFirst),
+			findViewById(R.id.avoidAfterimage)
+		};
+		
+		setDriverListener((cmd, sig, note, velo) -> {
+			if (usbDeviceConnection != null) {
+				if (mode == 0) {
+					try {
+						(new AsyncTask<String, Integer, String>() {
+							@Override
+							protected String doInBackground(String... params) {
+								sendBuffer(cmd, sig, note, velo);
+								return null;
+							}
+						}).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+					} catch (Exception ignore) {
+						logRecv("런치패드 led 에러");
+					}
+				} else if (mode == 1)
+					sendBuffer(cmd, sig, note, velo);
+			}
+		});
+	}
+	
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_usbmidi);
+		initVar();
+		
+		mode = SettingManager.LaunchpadConnectMethod.load(Launchpad.this);
+		
+		selectDevice(device);
+		selectMode(mode);
+		
+		Intent intent = getIntent();
+		usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+		usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+		if ("android.hardware.usb.action.USB_DEVICE_ATTACHED".equals(intent.getAction()))
+			initDevice(usbDevice);
+		else {
+			Iterator<UsbDevice> deviceIterator = ((UsbManager) Objects.requireNonNull(getSystemService(Context.USB_SERVICE))).getDeviceList().values().iterator();
+			if (deviceIterator.hasNext())
+				initDevice(deviceIterator.next());
+		}
+		
+		
+		(new Handler()).postDelayed(this::finish, 2000);
+	}
+	
+	
+	private void initDevice(UsbDevice device) {
+		
+		RL_err.setVisibility(View.GONE);
+		
+		int interfaceNum = 0;
+		
+		if (device == null) {
+			logRecv("USB 에러 : device == null");
+			return;
+		} else {
+			try {
+				logRecv("DeviceName : " + device.getDeviceName());
+				logRecv("DeviceClass : " + device.getDeviceClass());
+				logRecv("DeviceId : " + device.getDeviceId());
+				logRecv("DeviceProtocol : " + device.getDeviceProtocol());
+				logRecv("DeviceSubclass : " + device.getDeviceSubclass());
+				logRecv("InterfaceCount : " + device.getInterfaceCount());
+				logRecv("VendorId : " + device.getVendorId());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				logRecv("ProductId : " + device.getProductId());
+				TV_info.append("ProductId : " + device.getProductId() + "\n");
+				switch (device.getProductId()) {
+					case 8:
+						selectDevice(MidiFighter.value);
+						TV_info.append("prediction : MidiFighter\n");
+						break;
+					case 105:
+						selectDevice(MK2.value);
+						TV_info.append("prediction : MK2\n");
+						break;
+					case 81:
+						selectDevice(Pro.value);
+						TV_info.append("prediction : Pro\n");
+						break;
+					case 54:
+						selectDevice(S.value);
+						TV_info.append("prediction : mk2 mini\n");
+						break;
+					case 8211:
+						selectDevice(Piano.value);
+						TV_info.append("prediction : LX 61 piano\n");
+						break;
+					case 32822:
+						selectDevice(MK2.value);
+						TV_info.append("prediction : Arduino Leonardo midi\n");
+						interfaceNum = 3;
+						break;
+					default:
+						selectDevice(Piano.value);
+						TV_info.append("prediction : unknown\n");
+						break;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		for (int i = interfaceNum; i < device.getInterfaceCount(); i++) {
+			UsbInterface ui = device.getInterface(i);
+			if (ui.getEndpointCount() > 0) {
+				usbInterface = ui;
+				TV_info.append("Interface : (" + (i + 1) + "/" + device.getInterfaceCount() + ")\n");
+				break;
+			}
+		}
+		for (int i = 0; i < usbInterface.getEndpointCount(); i++) {
+			UsbEndpoint ep = usbInterface.getEndpoint(i);
+			if (ep.getDirection() == UsbConstants.USB_DIR_IN) {
+				TV_info.append("Endpoint_In : (" + (i + 1) + "/" + usbInterface.getEndpointCount() + ")\n");
+				usbEndpoint_in = ep;
+			} else if (ep.getDirection() == UsbConstants.USB_DIR_OUT) {
+				TV_info.append("Endpoint_OUT : (" + (i + 1) + "/" + usbInterface.getEndpointCount() + ")\n");
+				usbEndpoint_out = ep;
+			} else {
+				TV_info.append("Endpoint_Unknown : (" + (i + 1) + "/" + usbInterface.getEndpointCount() + ")\n");
+			}
+		}
+		usbDeviceConnection = usbManager.openDevice(device);
+		if (usbDeviceConnection == null) {
+			logRecv("USB 에러 : usbDeviceConnection == null");
+			return;
+		}
+		if (usbDeviceConnection.claimInterface(usbInterface, true)) {
+			(new ReceiveTask()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+		} else {
+			logRecv("USB 에러 : usbDeviceConnection.claimInterface(usbInterface, true)");
+		}
+	}
+	
+	// ========================================================================================= 설정 선택
+	
+	public void selectDeviceXml(View v) {
+		selectDevice(Integer.parseInt((String) v.getTag()));
+	}
+	
+	public void selectDevice(MidiDevice m) {
+		if (m != null)
+			selectDevice(m.value);
+	}
+	
+	public void selectDevice(int num) {
+		
+		switch (LL_Launchpad[num].getId()) {
+			case R.id.s:
+				device = S;
+				driver = new LaunchpadDriver.LaunchpadS();
+				break;
+			case R.id.mk2:
+				device = MK2;
+				driver = new LaunchpadDriver.LaunchpadMK2();
+				break;
+			case R.id.pro:
+				device = Pro;
+				driver = new LaunchpadDriver.LaunchpadPRO();
+				break;
+			case R.id.midifighter:
+				device = MidiFighter;
+				driver = new LaunchpadDriver.MidiFighter();
+				break;
+			case R.id.piano:
+				device = Piano;
+				driver = new LaunchpadDriver.Piano();
+				break;
+		}
+		
+		for (int i = 0; i < LL_Launchpad.length; i++) {
+			if (device.value == i) {
+				LL_Launchpad[i].setBackgroundColor(color(R.color.gray1));
+				int count = LL_Launchpad[i].getChildCount();
+				for (int j = 0; j < count; j++) {
+					TextView textView = (TextView) LL_Launchpad[i].getChildAt(j);
+					textView.setTextColor(color(R.color.background1));
+				}
+			} else {
+				LL_Launchpad[i].setBackgroundColor(color(R.color.background1));
+				int count = LL_Launchpad[i].getChildCount();
+				for (int j = 0; j < count; j++) {
+					TextView textView = (TextView) LL_Launchpad[i].getChildAt(j);
+					textView.setTextColor(color(R.color.gray1));
+				}
+			}
+		}
+		
+		updateDriver();
+	}
+	
+	
+	public void selectModeXml(View v) {
+		selectMode(Integer.parseInt((String) v.getTag()));
+	}
+	
+	public void selectMode(int num) {
+		
+		switch (LL_mode[num].getId()) {
+			case R.id.speedFirst:
+				mode = 0;
+				break;
+			case R.id.avoidAfterimage:
+				mode = 1;
+				break;
+		}
+		
+		for (int i = 0; i < LL_mode.length; i++) {
+			if (mode == i) {
+				LL_mode[i].setBackgroundColor(color(R.color.gray1));
+				int count = LL_mode[i].getChildCount();
+				for (int j = 0; j < count; j++) {
+					TextView textView = (TextView) LL_mode[i].getChildAt(j);
+					textView.setTextColor(color(R.color.background1));
+				}
+			} else {
+				LL_mode[i].setBackgroundColor(color(R.color.background1));
+				int count = LL_mode[i].getChildCount();
+				for (int j = 0; j < count; j++) {
+					TextView textView = (TextView) LL_mode[i].getChildAt(j);
+					textView.setTextColor(color(R.color.gray1));
+				}
+			}
+		}
+		
+		SettingManager.LaunchpadConnectMethod.save(Launchpad.this, mode);
+	}
+	
+	// ========================================================================================= ReceiveTask
+	
+	public static class ReceiveTask extends AsyncTask<String, Integer, String> {
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			driver.onConnected();
+		}
+		
+		@SuppressLint("DefaultLocale")
+		@Override
+		protected String doInBackground(String... params) {
+			if (!isRun) {
+				isRun = true;
+				logRecv("USB 시작");
+				
+				long prevTime = System.currentTimeMillis();
+				int count = 0;
+				byte[] byteArray = new byte[usbEndpoint_in.getMaxPacketSize()];
+				while (isRun) {
+					try {
+						int length = usbDeviceConnection.bulkTransfer(usbEndpoint_in, byteArray, byteArray.length, 1000);
+						if (length >= 4) {
+							for (int i = 0; i < length; i += 4) {
+								int cmd = byteArray[i];
+								int sig = byteArray[i + 1];
+								int note = byteArray[i + 2];
+								int velocity = byteArray[i + 3];
+								
+								publishProgress(cmd, sig, note, velocity);
+								logSig(String.format("%-7d%-7d%-7d%-7d", cmd, sig, note, velocity));
+							}
+						} else if (length == -1) {
+							long currTime = System.currentTimeMillis();
+							if (prevTime != currTime) {
+								count = 0;
+								prevTime = currTime;
+							} else {
+								count++;
+								if (count > 10)
+									break;
+							}
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						break;
+					}
+				}
+				
+				logRecv("USB 끝");
+			}
+			isRun = false;
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+//			try {
+			driver.getSignal(progress[0], progress[1], progress[2], progress[3]);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			driver.onDisconnected();
+		}
+	}
+	
+	
+	static void sendBuffer(byte cmd, byte sig, byte note, byte velocity) {
+		try {
+			byte[] buffer = {cmd, sig, note, velocity};
+			usbDeviceConnection.bulkTransfer(usbEndpoint_out, buffer, buffer.length, 1000);
+		} catch (Exception ignored) {
+		}
+	}
+	
+	
+	// ========================================================================================= Driver
+	
+	static void setDriverListener(Activity activity, LaunchpadDriver.DriverRef.OnConnectionEventListener listener1, LaunchpadDriver.DriverRef.OnGetSignalListener listener2) {
+		driverFrom = activity;
+		onConnectionEventListener = listener1;
+		onGetSignalListener = listener2;
+		
+		updateDriver();
+	}
+	
+	static void removeDriverListener(Activity activity) {
+		log("driverFrom == activity = " + (driverFrom == activity));
+		if (driverFrom == activity)
+			setDriverListener(null, null, null);
+	}
+	
+	static void setDriverListener(LaunchpadDriver.DriverRef.OnSendSignalListener listener) {
+		onSendSignalListener = listener;
+		
+		updateDriver();
+	}
+	
+	public static void updateDriver() {
+		logRecv("updateDriver");
+		driver.setOnConnectionEventListener(onConnectionEventListener);
+		driver.setOnGetSignalListener(onGetSignalListener);
+		driver.setOnSendSignalListener(onSendSignalListener);
+		if (isRun)
+			driver.onConnected();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		initVar();
+	}
+	
+	
+	@SuppressLint("StaticFieldLeak")
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+}
---
+++ kotlin/app/src/main/java/com/kimjisub/launchpad/manage/LaunchpadDriver.java
@@ -0,0 +1,477 @@
+package com.kimjisub.launchpad.manage;
+
+import static com.kimjisub.launchpad.manage.Tools.logRecv;
+
+public class LaunchpadDriver {
+	
+	public static abstract class DriverRef {
+		
+		// ================================================================================= OnConnectionEventListener
+		
+		private OnConnectionEventListener onConnectionEventListener = null;
+		
+		public interface OnConnectionEventListener {
+			void onConnected();
+			
+			void onDisconnected();
+		}
+		
+		public DriverRef setOnConnectionEventListener(OnConnectionEventListener listener) {
+			onConnectionEventListener = listener;
+			return this;
+		}
+		
+		public void onConnected() {
+			logRecv("onConnected");
+			if (onConnectionEventListener != null)
+				onConnectionEventListener.onConnected();
+		}
+		
+		public void onDisconnected() {
+			logRecv("onDisconnected");
+			if (onConnectionEventListener != null)
+				onConnectionEventListener.onDisconnected();
+		}
+		
+		// ================================================================================= OnGetSignalListener
+		
+		private OnGetSignalListener onGetSignalListener = null;
+		
+		public interface OnGetSignalListener {
+			void onPadTouch(int x, int y, boolean upDown, int velo);
+			
+			void onFunctionkeyTouch(int f, boolean upDown);
+			
+			void onChainTouch(int c, boolean upDown);
+			
+			void onUnknownEvent(int cmd, int sig, int note, int velo);
+		}
+		
+		public DriverRef setOnGetSignalListener(OnGetSignalListener listener) {
+			onGetSignalListener = listener;
+			return this;
+		}
+		
+		void onPadTouch(int x, int y, boolean upDown, int velo) {
+			logRecv("onPadTouch(" + x + ", " + y + ", " + upDown + ", " + velo + ")");
+			if (onGetSignalListener != null)
+				onGetSignalListener.onPadTouch(x, y, upDown, velo);
+		}
+		
+		void onFunctionkeyTouch(int f, boolean upDown) {
+			logRecv("onFunctionkeyTouch(" + f + ", " + upDown + ")");
+			if (onGetSignalListener != null)
+				onGetSignalListener.onFunctionkeyTouch(f, upDown);
+		}
+		
+		void onChainTouch(int c, boolean upDown) {
+			logRecv("onChainTouch(" + c + ", " + upDown + ")");
+			if (onGetSignalListener != null)
+				onGetSignalListener.onChainTouch(c, upDown);
+		}
+		
+		void onUnknownEvent(int cmd, int sig, int note, int velo) {
+			logRecv("onUnknownEvent(" + cmd + ", " + sig + ", " + note + ", " + velo + ")");
+			if (onGetSignalListener != null)
+				onGetSignalListener.onUnknownEvent(cmd, sig, note, velo);
+		}
+		
+		public abstract void getSignal(int cmd, int sig, int note, int velo);
+		
+		
+		// ================================================================================= OnSendSignalListener
+		
+		private OnSendSignalListener onSendSignalListener = null;
+		
+		public interface OnSendSignalListener {
+			void onSend(final byte cmd, final byte sig, final byte note, final byte velo);
+		}
+		
+		public DriverRef setOnSendSignalListener(OnSendSignalListener listener) {
+			onSendSignalListener = listener;
+			return this;
+		}
+		
+		void onSend(final byte cmd, final byte sig, final byte note, final byte velo) {
+			if (onSendSignalListener != null)
+				onSendSignalListener.onSend(cmd, sig, note, velo);
+		}
+		
+		public void sendSignal(int cmd, int sig, int note, int velo) {
+			onSend((byte) cmd, (byte) sig, (byte) note, (byte) velo);
+		}
+		
+		public void send09Signal(final int note, final int velo) {
+			sendSignal((byte) 9, (byte) -112, (byte) note, (byte) velo);
+		}
+		
+		public void send11Signal(final int note, final int velo) {
+			sendSignal((byte) 11, (byte) -80, (byte) note, (byte) velo);
+		}
+		
+		public abstract void sendPadLED(int x, int y, int velo);
+		
+		public abstract void sendChainLED(int c, int velo);
+		
+		public abstract void sendFunctionkeyLED(int f, int velo);
+		
+		public abstract void sendClearLED();
+		
+	}
+	
+	public static class Nothing extends DriverRef {
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+		}
+		
+		@Override
+		public void sendClearLED() {
+		}
+		
+	}
+	
+	public static class LaunchpadS extends DriverRef {
+		static final int[][] circleCode = {
+			{11, -80, 104},
+			{11, -80, 105},
+			{11, -80, 106},
+			{11, -80, 107},
+			{11, -80, 108},
+			{11, -80, 109},
+			{11, -80, 110},
+			{11, -80, 111},
+			{9, -112, 8},
+			{9, -112, 24},
+			{9, -112, 40},
+			{9, -112, 56},
+			{9, -112, 72},
+			{9, -112, 88},
+			{9, -112, 104},
+			{9, -112, 120},
+		};
+		
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+			if (cmd == 9) {
+				int x = note / 16 + 1;
+				int y = note % 16 + 1;
+				
+				if (y >= 1 && y <= 8)
+					onPadTouch(x - 1, y - 1, velo != 0, velo);
+				else if (y == 9) {
+					onChainTouch(x - 1, velo != 0);
+					onFunctionkeyTouch(x - 1 + 8, velo != 0);
+				}
+			} else if (cmd == 11) {
+				if (104 <= note && note <= 111)
+					onFunctionkeyTouch(note - 104, velo != 0);
+			}
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+			send09Signal(x * 16 + y, LaunchpadColor.SCode[velo]);
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+			if (0 <= c && c <= 7)
+				sendFunctionkeyLED(c + 8, velo);
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+			if (0 <= f && f <= 15)
+				sendSignal((byte) circleCode[f][0], (byte) circleCode[f][1], (byte) circleCode[f][2], (byte) LaunchpadColor.SCode[velo]);
+		}
+		
+		@Override
+		public void sendClearLED() {
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+					sendPadLED(i, j, 0);
+			for (int i = 0; i < 16; i++)
+				sendFunctionkeyLED(i, 0);
+		}
+	}
+	
+	public static class LaunchpadMK2 extends DriverRef {
+		static final int[][] circleCode = {
+			{11, -80, 104},
+			{11, -80, 105},
+			{11, -80, 106},
+			{11, -80, 107},
+			{11, -80, 108},
+			{11, -80, 109},
+			{11, -80, 110},
+			{11, -80, 111},
+			{9, -112, 89},
+			{9, -112, 79},
+			{9, -112, 69},
+			{9, -112, 59},
+			{9, -112, 49},
+			{9, -112, 39},
+			{9, -112, 29},
+			{9, -112, 19},
+		};
+		
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+			if (cmd == 9) {
+				int x = 9 - (note / 10);
+				int y = note % 10;
+				
+				if (y >= 1 && y <= 8)
+					onPadTouch(x - 1, y - 1, velo != 0, velo);
+				else if (y == 9) {
+					onChainTouch(x - 1, velo != 0);
+					onFunctionkeyTouch(x - 1 + 8, velo != 0);
+				}
+			} else if (cmd == 11) {
+				if (104 <= note && note <= 111)
+					onFunctionkeyTouch(note - 104, velo != 0);
+			}
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+			send09Signal(10 * (8 - x) + y + 1, velo);
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+			if (0 <= c && c <= 7)
+				sendFunctionkeyLED(c + 8, velo);
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+			if (0 <= f && f <= 15)
+				sendSignal((byte) circleCode[f][0], (byte) circleCode[f][1], (byte) circleCode[f][2], (byte) velo);
+		}
+		
+		@Override
+		public void sendClearLED() {
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+					sendPadLED(i, j, 0);
+			for (int i = 0; i < 16; i++)
+				sendFunctionkeyLED(i, 0);
+		}
+	}
+	
+	public static class LaunchpadPRO extends DriverRef {
+		static final int[][] circleCode = {
+			{11, -80, 91},
+			{11, -80, 92},
+			{11, -80, 93},
+			{11, -80, 94},
+			{11, -80, 95},
+			{11, -80, 96},
+			{11, -80, 97},
+			{11, -80, 98},
+			{11, -80, 89},
+			{11, -80, 79},
+			{11, -80, 69},
+			{11, -80, 59},
+			{11, -80, 49},
+			{11, -80, 39},
+			{11, -80, 29},
+			{11, -80, 19},
+			{11, -80, 8},
+			{11, -80, 7},
+			{11, -80, 6},
+			{11, -80, 5},
+			{11, -80, 4},
+			{11, -80, 3},
+			{11, -80, 2},
+			{11, -80, 1},
+			{11, -80, 10},
+			{11, -80, 20},
+			{11, -80, 30},
+			{11, -80, 40},
+			{11, -80, 50},
+			{11, -80, 60},
+			{11, -80, 70},
+			{11, -80, 80}
+		};
+		
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+			if (cmd == 9) {
+				int x = 9 - (note / 10);
+				int y = note % 10;
+				
+				if (y >= 1 && y <= 8)
+					onPadTouch(x - 1, y - 1, velo != 0, velo);
+			}
+			if (cmd == 11 && sig == -80) {
+				if (91 <= note && note <= 98) {
+					onFunctionkeyTouch(note - 91, velo != 0);
+				}
+				if (19 <= note && note <= 89 && note % 10 == 9) {
+					int c = 9 - (note / 10) - 1;
+					onChainTouch(c, velo != 0);
+					onFunctionkeyTouch(c + 8, velo != 0);
+				}
+				if (1 <= note && note <= 8) {
+					onChainTouch(8 - note + 16 -8, velo != 0);
+					onFunctionkeyTouch(8 - note + 16, velo != 0);
+				}
+				if (10 <= note && note <= 80 && note % 10 == 0) {
+					onChainTouch((note / 10) - 1 + 24-8, velo != 0);
+					onFunctionkeyTouch((note / 10) - 1 + 24, velo != 0);
+				}
+				
+			} else if (cmd == 7 && sig == 46 && velo == -9)
+				logRecv("PRO >??");
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+			send09Signal(10 * (8 - x) + y + 1, velo);
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+			if (0 <= c && c <= 7)
+				sendFunctionkeyLED(c + 8, velo);
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+			if (0 <= f && f <= 31)
+				sendSignal((byte) circleCode[f][0], (byte) circleCode[f][1], (byte) circleCode[f][2], (byte) velo);
+		}
+		
+		
+		@Override
+		public void sendClearLED() {
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+					sendPadLED(i, j, 0);
+			for (int i = 0; i < 32; i++)
+				sendFunctionkeyLED(i, 0);
+		}
+	}
+	
+	public static class MidiFighter extends DriverRef {
+		
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+			
+			int x;
+			int y;
+			if (cmd == 9) {
+				if (note >= 36 && note <= 67) {
+					x = (67 - note) / 4 + 1;
+					y = 4 - (67 - note) % 4;
+					onPadTouch(x - 1, y - 1, true, velo);
+				} else if (note >= 68 && note <= 99) {
+					x = (99 - note) / 4 + 1;
+					y = 8 - (99 - note) % 4;
+					onPadTouch(x - 1, y - 1, true, velo);
+				}
+				
+			} else if (cmd == 8) {
+				if (note >= 36 && note <= 67) {
+					x = (67 - note) / 4 + 1;
+					y = 4 - (67 - note) % 4;
+					onPadTouch(x - 1, y - 1, false, velo);
+				} else if (note >= 68 && note <= 99) {
+					x = (99 - note) / 4 + 1;
+					y = 8 - (99 - note) % 4;
+					onPadTouch(x - 1, y - 1, false, velo);
+				}
+				
+			}
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+			x += 1;
+			y += 1;
+			
+			if (1 <= y && y <= 4)
+				sendSignal(9, -110, (-4) * x + y + 67, velo);
+			else if (5 <= y && y <= 8)
+				sendSignal(9, -110, (-4) * x + y + 95, velo);
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+		}
+		
+		@Override
+		public void sendClearLED() {
+			for (int i = 0; i < 8; i++)
+				for (int j = 0; j < 8; j++)
+					sendPadLED(i, j, 0);
+		}
+	}
+	
+	public static class Piano extends DriverRef {
+		@Override
+		public void getSignal(int cmd, int sig, int note, int velo) {
+			
+			int x;
+			int y;
+			
+			if (cmd == 9) {
+				if (note >= 36 && note <= 67) {
+					x = (67 - note) / 4 + 1;
+					y = 4 - (67 - note) % 4;
+					onPadTouch(x - 1, y - 1, velo != 0, velo);
+				} else if (note >= 68 && note <= 99) {
+					x = (99 - note) / 4 + 1;
+					y = 8 - (99 - note) % 4;
+					onPadTouch(x - 1, y - 1, velo != 0, velo);
+				}
+				
+			} else if (velo == 0) {
+				if (note >= 36 && note <= 67) {
+					x = (67 - note) / 4 + 1;
+					y = 4 - (67 - note) % 4;
+					onPadTouch(x - 1, y - 1, false, 0);
+				} else if (note >= 68 && note <= 99) {
+					x = (99 - note) / 4 + 1;
+					y = 8 - (99 - note) % 4;
+					onPadTouch(x - 1, y - 1, false, 0);
+				}
+			}
+		}
+		
+		@Override
+		public void sendPadLED(int x, int y, int velo) {
+		}
+		
+		@Override
+		public void sendChainLED(int c, int velo) {
+		}
+		
+		@Override
+		public void sendFunctionkeyLED(int f, int velo) {
+		}
+		
+		@Override
+		public void sendClearLED() {
+		}
+		
+	}
+}
