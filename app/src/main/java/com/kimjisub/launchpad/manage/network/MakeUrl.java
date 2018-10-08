0d374c47e85f47baf2a94e41a5d71227e41c68b0
김지섭 <0226daniel@gmail.com>
2018-10-08 18:00:41 +0900

    public

--- app/src/main/java/com/kimjisub/launchpad/manage/network/MakeUrl.java
+++ app/src/main/java/com/kimjisub/launchpad/manage/network/MakeUrl.java
@@ -1,18 +1,18 @@
 package com.kimjisub.launchpad.manage.network;
 
 public class MakeUrl {
-	Integer id;
-	String code;
-	String title;
-	String author;
-	String content;
-	String site;
-	String url;
-	Integer fileSize;
-	boolean isPublic;
-	String passwd;
-	Integer downloadCount;
-	Boolean deleted;
-	String createdAt;
-	String updatedAt;
+	public Integer id;
+	public String code;
+	public String title;
+	public String author;
+	public String content;
+	public String site;
+	public String url;
+	public Integer fileSize;
+	public boolean isPublic;
+	public String passwd;
+	public Integer downloadCount;
+	public Boolean deleted;
+	public String createdAt;
+	public String updatedAt;
 }
