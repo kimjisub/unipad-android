d442d9c406ddbec9c5381d849fdb4c3527e04ce6
0226daniel <0226daniel@gmail.com>
2019-02-25 00:38:25 +0900

    RecycleView apply

---
+++ app/src/main/java/com/kimjisub/launchpad/listManager/MainAdapter.java
@@ -0,0 +1,101 @@
+package com.kimjisub.launchpad.listManager;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.kimjisub.launchpad.MainActivity;
+import com.kimjisub.launchpad.R;
+import com.kimjisub.launchpad.db.manager.DB_Unipack;
+import com.kimjisub.launchpad.db.vo.UnipackVO;
+import com.kimjisub.unipad.designkit.PackViewSimple;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.UniPackHolder> {
+
+	private ArrayList<MainItem> list;
+	private DB_Unipack DB_unipack;
+	private MainActivity context;
+
+	public MainAdapter(ArrayList list, DB_Unipack DB_unipack, MainActivity context) {
+		this.list = list;
+		this.DB_unipack = DB_unipack;
+		this.context = context;
+	}
+
+	@Override
+	public UniPackHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		PackViewSimple packViewSimple = new PackViewSimple(parent.getContext());
+		final LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		int left = context.dpToPx(16);
+		int top = 0;
+		int right = context.dpToPx(16);
+		int bottom = context.dpToPx(10);
+		lp.setMargins(left, top, right, bottom);
+		packViewSimple.setLayoutParams(lp);
+		UniPackHolder uniPackHolder = new UniPackHolder(packViewSimple);
+
+		return uniPackHolder;
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull UniPackHolder uniPackHolder, int position) {
+		MainItem item = list.get(position);
+		PackViewSimple packViewSimple = uniPackHolder.packViewSimple;
+
+		UnipackVO unipackVO = DB_unipack.getOrCreateByPath(item.path);
+
+		String title = item.unipack.title;
+		String subTitle = item.unipack.producerName;
+
+		if (item.unipack.CriticalError) {
+			item.flagColor = context.color(R.color.red);
+			title = context.lang(R.string.errOccur);
+			subTitle = item.path;
+		} else
+			item.flagColor = context.color(R.color.skyblue);
+
+		if (unipackVO.bookmark)
+			item.flagColor = context.color(R.color.orange);
+
+
+		packViewSimple
+				.setFlagColor(item.flagColor)
+				.setTitle(title)
+				.setSubTitle(subTitle)
+				.setOption1(context.lang(R.string.LED_), item.unipack.isKeyLED)
+				.setOption2(context.lang(R.string.autoPlay_), item.unipack.isAutoPlay)
+				.setOnEventListener(new PackViewSimple.OnEventListener() {
+					@Override
+					public void onViewClick(PackViewSimple v) {
+						context.togglePlay(item.path);
+					}
+
+					@Override
+					public void onViewLongClick(PackViewSimple v) {
+					}
+
+					@Override
+					public void onPlayClick(PackViewSimple v) {
+						context.pressPlay(item.path);
+					}
+				});
+		item.packViewSimple = packViewSimple;
+	}
+
+	@Override
+	public int getItemCount() {
+		return list.size();
+	}
+
+	public static class UniPackHolder extends RecyclerView.ViewHolder {
+		PackViewSimple packViewSimple;
+
+		UniPackHolder(PackViewSimple view) {
+			super(view);
+			packViewSimple = view;
+		}
+	}
+}
