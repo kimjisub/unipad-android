b1e572656ce70328c28468e3347a93945383f0e7
0226daniel <0226daniel@gmail.com>
2019-09-14 22:25:46 +0900

    SyncCheckBox -> Kotlin

---
+++ design/src/main/java/com/kimjisub/design/manage/SyncCheckBox.kt
@@ -0,0 +1,98 @@
+package com.kimjisub.design.manage
+
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import java.util.*
+
+class SyncCheckBox(vararg cbs: CheckBox) {
+	private val checkBoxes: ArrayList<CheckBox> = ArrayList()
+
+	init {
+		for (cb in cbs)
+			addCheckBox(cb)
+	}
+
+	fun addCheckBox(checkBox: CheckBox) {
+		checkBox.setOnCheckedChangeListener { _: CompoundButton?, b: Boolean -> setChecked(b) }
+		checkBox.setOnLongClickListener {
+			onLongClick()
+			false
+		}
+		checkBoxes.add(checkBox)
+	}
+
+	// Checked Manage /////////////////////////////////////////////////////////////////////////////////////////
+
+	private var isChecked = false
+	var isLocked: Boolean = false
+		set(value) {
+			field = value
+			for (checkBox in checkBoxes)
+				checkBox.isEnabled = !field
+		}
+
+	fun isChecked() = isChecked
+
+	fun toggleChecked() {
+		if (!isLocked)
+			forceToggleChecked()
+	}
+
+
+	fun setChecked(b: Boolean) {
+		if (!isLocked)
+			forceSetChecked(b)
+		else
+			fix()
+	}
+
+
+	fun forceToggleChecked() = forceSetChecked(!isChecked)
+
+	fun forceSetChecked(b: Boolean) {
+		isChecked = b
+		fix()
+		onCheckedChange(b)
+	}
+
+	fun fix() {
+		for (checkBox in checkBoxes) {
+			if (checkBox.isChecked != isChecked) {
+				checkBox.setOnCheckedChangeListener(null)
+				checkBox.isChecked = isChecked
+				checkBox.setOnCheckedChangeListener { _: CompoundButton?, b: Boolean -> setChecked(b) }
+			}
+		}
+	}
+
+
+	// view /////////////////////////////////////////////////////////////////////////////////////////
+
+
+	fun setVisibility(visibility: Int) {
+		for (checkBox in checkBoxes) checkBox.visibility = visibility
+	}
+
+
+	// listener /////////////////////////////////////////////////////////////////////////////////////////
+
+	var onCheckedChange: OnCheckedChange? = null
+
+	var onLongClick: OnLongClick? = null
+
+	interface OnCheckedChange {
+		fun onCheckedChange(b: Boolean)
+	}
+
+	interface OnLongClick {
+		fun onLongClick()
+	}
+
+	private fun onCheckedChange(bool: Boolean) {
+		onCheckedChange?.onCheckedChange(bool)
+	}
+
+	private fun onLongClick() {
+		onLongClick?.onLongClick()
+	}
+}
