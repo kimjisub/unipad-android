efae89161a9a287408cf5713ec199100df582203
0226daniel <0226daniel@gmail.com>
2019-09-16 22:06:41 +0900

    Change FsStore to Kotlin

---
+++ app/src/main/java/com/kimjisub/launchpad/network/fb/FsStore.kt
@@ -0,0 +1,30 @@
+package com.kimjisub.launchpad.network.fb
+
+import com.google.firebase.database.IgnoreExtraProperties
+import java.util.*
+
+@IgnoreExtraProperties
+class FsStore {
+	var chainCount: Long = 0
+	var code: String? = null
+	var description: String? = null
+	var difficulty: Long = 0
+	var downloadCount: Long = 0
+	@JvmField
+	var isAutoPlay = false
+	@JvmField
+	var isLED = false
+	@JvmField
+	var isNew = false
+	@JvmField
+	var isProLight = false
+	@JvmField
+	var isWormhole = false
+	var playTime: Long = 0
+	var producerName: String? = null
+	var rank: Long = 0
+	var title: String? = null
+	var uploadAt: Date? = null
+	var url: String? = null
+	var website: String? = null
+}
