37fb91361bd17e16a66137be4b0f114adaccd1ac
0226daniel <0226daniel@gmail.com>
2019-09-16 22:57:21 +0900

    FSStoreActivity -> Kotlin

---
+++ app/src/main/java/com/kimjisub/launchpad/activity/FSStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.kimjisub.launchpad.activity
+
+import android.os.Bundle
+import com.kimjisub.launchpad.R.layout
+
+class FSStoreActivity : BaseActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(layout.activity_store)
+	}
+}
