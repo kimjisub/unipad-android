e103298362bd8075d7d69368fa0c92cc124eeeaa
0226daniel <0226daniel@gmail.com>
2019-09-18 18:34:55 +0900

    Constant -> Kotlin

---
+++ app/src/main/java/com/kimjisub/launchpad/manager/Constant.kt
@@ -0,0 +1,30 @@
+package com.kimjisub.launchpad.manager
+
+object Constant {
+	const val AUTOPLAY_AUTOMAPPING_DELAY_PRESET = -15
+	const val ADSCOOLTIME = 60000
+
+	object BILLING {
+		const val DEVELOPERPAYLOAD =
+			"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkLmP+mnbAw6WKHih7dKPiLu4XFIu1+MsufPIp6yLRzMM48U+1k8juultRjvOX68O46LTboCnNlfjUWPA/2GhiuVofvDK2KmUXXWgXkihml9R5PiVX17JysoQvfHwPtuCXaBib6ng9vHOosBIG1mCs53pQJ7/0R94fb2BgQ2uX4QCBGZ4D+tOULBpOz5U1wtQhrIP5gpe8NQ9J/UGjz6hs1QXp5RBJVCYbXo/DZCzK1pIfG9dDUx1yDxWfSvspqdpwbL45O9p2hMvtUFL6jSzuC9bqenZEaqb0fj7YDdNI1e8wfmooyHAJJxvpV2FnspuYZ7nbG1wLoGW9xKtL5yjOwIDAQAB"
+		const val REMOVE_ADS = "v1_remove_ads"
+		const val PRO_TOOLS = "v1_pro_tools"
+		const val DONATE_1 = "donation_1"
+		const val DONATE_5 = "donation_5"
+		const val DONATE_10 = "donation_10"
+		const val DONATE_50 = "donation_50"
+		const val DONATE_100 = "donation_100"
+	}
+
+	object ADMOB {
+		const val MAIN_START = "ca-app-pub-1077445788578961/6843593938"
+		const val MAIN_LIST = "ca-app-pub-1077445788578961/6318023505"
+		const val PLAY_END = "ca-app-pub-1077445788578961/6242001881"
+	}
+
+	object VUNGLE {
+		const val APPID = "5bb765239e0522795b6641af"
+		const val MAIN_INFEED = "18426-4958348"
+		const val PLAY_END = "44597-8583205"
+	}
+}
