0db97d84967737a693ceeea070c68ea8b9841bcf
김지섭 <0226daniel@gmail.com>
2020-08-03 20:41:30 +0900

    fix: Rename FBStore to Store

--- app/src/main/java/com/kimjisub/launchpad/activity/StoreActivity.kt
+++ app/src/main/java/com/kimjisub/launchpad/activity/StoreActivity.kt
@@ -3,7 +3,7 @@ package com.kimjisub.launchpad.activity
 import android.os.Bundle
 import com.kimjisub.launchpad.R.layout
 
-class FSStoreActivity : BaseActivity() {
+class StoreActivity : BaseActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(layout.activity_store)
