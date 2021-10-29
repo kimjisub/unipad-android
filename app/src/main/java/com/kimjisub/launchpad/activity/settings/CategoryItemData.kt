657f4a20bce7066add9435ca1f2d8012c01ec548
김지섭 <0226daniel@gmail.com>
2021-10-29 22:10:14 +0900

    Revert "feat: new SettingsActivity"
    This reverts commit a7ae87bb14218adab2aa4cece016e2f77510218f.

--- app/src/main/java/com/kimjisub/launchpad/activity/settings/CategoryItemData.kt
+++
@@ -1,9 +0,0 @@
-package com.kimjisub.launchpad.activity.settings
-
-import androidx.preference.PreferenceFragmentCompat
-
-data class CategoryItemData(
-	val title: String,
-	val icon: Int,
-	val FragmentCreator: () -> PreferenceFragmentCompat
-)
