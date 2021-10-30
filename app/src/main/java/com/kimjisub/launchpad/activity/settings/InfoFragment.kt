e8a7b489440d5fbc1aeca68c38f8a6634524a72c
김지섭 <0226daniel@gmail.com>
2021-10-30 19:40:57 +0900

    feat: oss licence

--- app/src/main/java/com/kimjisub/launchpad/activity/settings/InfoFragment.kt
+++ app/src/main/java/com/kimjisub/launchpad/activity/settings/InfoFragment.kt
@@ -1,11 +1,23 @@
 package com.kimjisub.launchpad.activity.settings
 
 import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.kimjisub.launchpad.R
+import splitties.activities.start
 
 class InfoFragment : PreferenceFragmentCompat() {
+	private val ossLicencePreferenceCategory: Preference by lazy { findPreference("oss_licence")!! }
+
 	override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 		setPreferencesFromResource(R.xml.preferences_info, rootKey)
+
+		ossLicencePreferenceCategory.setOnPreferenceClickListener {
+			requireActivity().start<OssLicensesMenuActivity>()
+			true
+		}
 	}
 }
