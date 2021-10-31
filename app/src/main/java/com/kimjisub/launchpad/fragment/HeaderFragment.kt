5fe63f40b28da4e05f944e7548bcdf56042e15e9
김지섭 <0226daniel@gmail.com>
2021-10-31 20:44:51 +0900

    feat: change SetttingsActivity works

--- app/src/main/java/com/kimjisub/launchpad/fragment/HeaderFragment.kt
+++ app/src/main/java/com/kimjisub/launchpad/fragment/HeaderFragment.kt
@@ -1,11 +1,46 @@
 package com.kimjisub.launchpad.fragment
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.kimjisub.launchpad.R
+import com.kimjisub.launchpad.tool.Log
 
-class HeaderFragment : PreferenceFragmentCompat() {
+class HeaderFragment(val callback: (Category) -> Unit) : PreferenceFragmentCompat() {
+	private val infoPreference: Preference by lazy { findPreference("info")!! }
+	private val adsPaymentPreference: Preference by lazy { findPreference("ads_payment")!! }
+	private val storagePreference: Preference by lazy { findPreference("storage")!! }
+	private val themePreference: Preference by lazy { findPreference("theme")!! }
+
+	private val preferences by lazy {
+		arrayOf(infoPreference, adsPaymentPreference, storagePreference, themePreference)
+	}
+
 	override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 		setPreferencesFromResource(R.xml.preferences_header, rootKey)
+
+		select = 0
+		preferences.forEachIndexed { index, preference ->
+			preference.setOnPreferenceClickListener {
+				select = index
+				true
+			}
+		}
+	}
+
+	var select: Int = 0
+		set(value) {
+			field = value
+			preferences.forEachIndexed { index, preference ->
+				if (field == index) {
+					callback(Category.valueOf(preference.key.uppercase()))
+				}
+			}
+		}
+
+	companion object{
+		enum class Category(val key: String){
+			INFO("info"), ADS_PAYMENT("ads_payment"), STORAGE("storage"), THEME("theme")
+		}
 	}
 }
