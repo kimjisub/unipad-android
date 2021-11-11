6515a13c810b6a146b2381aa0affd08a24555106
김지섭 <0226daniel@gmail.com>
2021-11-11 12:43:16 +0900

    feat: fallbackToDestructiveMigration

--- app/src/main/java/com/kimjisub/launchpad/db/AppDatabase.kt
+++ app/src/main/java/com/kimjisub/launchpad/db/AppDatabase.kt
@@ -5,8 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.kimjisub.launchpad.db.dao.UnipackDao
 import com.kimjisub.launchpad.db.ent.Unipack
 import com.kimjisub.launchpad.db.util.DateConverter
@@ -19,20 +17,13 @@ abstract class AppDatabase : RoomDatabase() {
 	companion object {
 		private var INSTANCE: AppDatabase? = null
 
-		private val MIGRATION_1_2 = object : Migration(1, 2) {
-			override fun migrate(database: SupportSQLiteDatabase) {
-				database.execSQL("DROP TABLE UniPackENT")
-				database.execSQL("DROP TABLE UniPackOpenENT")
-			}
-		}
-
 		fun getInstance(context: Context): AppDatabase? {
 			if (INSTANCE == null) {
 				synchronized(AppDatabase::class) {
 					INSTANCE = Room.databaseBuilder(
 						context.applicationContext,
 						AppDatabase::class.java, "UniPad.db"
-					).addMigrations(MIGRATION_1_2).build()
+					).fallbackToDestructiveMigration().build()
 				}
 			}
 			return INSTANCE
